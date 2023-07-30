a34dcad0ff17991ef03e1da37676bc9ee2b8a986
N3dal <nedalxzo@gmail.com>
2023-07-30 16:48:05 +0200

    created main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------------
+#
+#
+#
+#
+# Author:N84.
+#
+# Create Date:Sun Jul 30 16:45:40 2023.
+# ///
+# ///
+# ///
+# -----------------------------------------------------------------
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from sys import argv
+
+
+def main():
+    
+    app = QApplication(argv)
+    
+    
+    exit(app.exec())
+    
+    
+if __name__ == "__main__":
+    main()
