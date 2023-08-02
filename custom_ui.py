8c14bfb920899d0c845ad4a03b0832af6810d03e
N3dal <nedalxzo@gmail.com>
2023-08-02 11:53:53 +0200

    added custom_ui.py

---
+++ custom_ui.py
@@ -0,0 +1,28 @@
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from defaults import *
+import sys
+
+
+class DataViewWidget(QWidget):
+    """
+        used to view the weather data;
+    """
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        pass
+    
+
+class SearchWidget(QWidget):
+    """
+        Docstring;
+    """
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        pass
